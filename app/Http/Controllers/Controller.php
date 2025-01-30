e1a9c685241f82fdfd4ab851d2e8e8a84f746431
= <=>
2025-01-30 16:30:22 +0100

    table

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,11 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Visit;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use PhpParser\Builder\Function_;
+use PhpParser\Node\Expr\FuncCall;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function showIndex()
+    {
+        $visit = Visit::find(1);
+        $visit->view = $visit->view + 1;
+        $visit->save();
+        return view('welcome');
+    }
 }
