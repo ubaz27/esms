909c5c42e19bbf1bcbcf9f91bc97185fe6b5345a
= <=>
2025-01-30 16:09:37 +0100

    sadf

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -877,7 +877,7 @@
 
                             <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Introduction To Excel
                             </h2>
-                            <iframe width="560" height="315"
+                            <iframe width="450" height="315"
                                 src="https://www.youtube.com/embed/L4u3T8SL7fc?si=EBsAk-xSi2gGrzXW"
                                 title="YouTube video player" frameborder="0"
                                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
@@ -888,11 +888,11 @@
                             </p>
                         </div>
 
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        {{-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                             class="self-center shrink-0 stroke-red-500 w-6 h-6 mx-6">
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
-                        </svg>
+                        </svg> --}}
                     </a>
 
 
