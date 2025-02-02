56e5da3b679c8dc3a31de85514e7411ddaba2141
= <=>
2025-02-02 13:48:12 +0100

    @2@

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -827,6 +827,28 @@
                 padding: 2rem
             }
         }
+
+
+        .float-container {
+            border: 1px solid;
+            padding: 2px;
+            height: 80px;
+        }
+
+        .float-child {
+            width: 49%;
+            float: left;
+            padding: 2px;
+            border: 2px;
+        }
+
+        .float-container span {
+            font-size: 18px;
+            padding-right: 30px;
+            color: darkgreen;
+            padding-left: 30px;
+
+        }
     </style>
 </head>
 
@@ -861,8 +883,8 @@
                 </svg>
             </div>
 
-            <div class="mt-16">
-                <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
+            <div class="float-container">
+                <div class="float-child">
                     <a href="https://youtu.be/L4u3T8SL7fc"
                         class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                         <div>
@@ -902,6 +924,41 @@
 
 
                 </div>
+
+                <div class="float-child">
+                    <a href="https://www.youtube.com/watch?v=oViZ_0htZsA"
+                        class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+                        <div>
+                            <div
+                                class="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" class="w-7 h-7 stroke-red-500">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
+                                </svg>
+                            </div>
+
+                            <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Data Entry </h2>
+                            <iframe width="450" height="315"
+                                src="https://www.youtube.com/embed/oViZ_0htZsA?si=SvDVoG7rgVTmHQtX"
+                                title="YouTube video player" frameborder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+                            <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+                                Excel Data Entry
+                            <p style="color:white;">Visits: {{ $no }}</p>
+                            </p>
+
+                        </div>
+
+                        {{-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            class="self-center shrink-0 stroke-red-500 w-6 h-6 mx-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
+                        </svg> --}}
+                    </a>
+                </div>
             </div>
 
             <div class="flex justify-center mt-16 px-0 sm:items-center sm:justify-between">
@@ -909,11 +966,11 @@
                     &nbsp;
                 </div>
 
-                <div class="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0">
-                    Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
-                </div>
+
             </div>
         </div>
+
+
     </div>
 </body>
 
